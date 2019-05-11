6f7076603153fe033ed74e12b505f786ecff33f9
Glitch (hello-express) <none>
2019-05-11 20:29:28 +0000

    🌄🌲 Checkpoint ./game.js:1065824/264

--- game.js
+++ game.js
@@ -1,10 +1,21 @@
+const gameSize = {
+  x: 800,
+  y: 524
+}
+
 const Player = require('./player')
+const utils = require('./utils')
 
 // meh
-const PlayerList = []
+const playersById = {}
 
 function addPlayer() {
   const player = new Player(
     `player${new Date().valueOf() - 1557600000000}`, // id
+    utils.randCoords(gameSize)
   )
+  
+  playersById[player.id] = player
+  
+  return player
 }
