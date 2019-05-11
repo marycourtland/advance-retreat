206022136660e37ce28ff12aa84de6071401e9ef
Glitch (hello-express) <none>
2019-05-11 18:01:23 +0000

    👱🚈 Checkpoint ./server.js:1065824/233

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const app = express();
 
 app.use(express.static('public'));
 
+// mobile client
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/solo.html');
+});
+
+// main game
 app.get('/game', function(request, response) {
   response.sendFile(__dirname + '/views/game.html');
 });
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/solo.html');
-});
 
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
