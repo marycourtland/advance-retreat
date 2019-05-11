b5bf02a2ce0830f777dd7b635a4159e998538308
Glitch (hello-express) <none>
2019-05-11 19:04:24 +0000

    🎽🎂 Checkpoint ./public/game.js:1065824/3061 ./public/group.js:1065824/1698

--- public/game.js
+++ public/game.js
@@ -2,7 +2,7 @@
 
 /* global fabric, Game */
 
-function() {
+(function() {
 
 
   const gameSize = {x: 800, y: 524}
@@ -12,80 +12,102 @@ function() {
   const paintRate = 5 // paints per second
   const dotSize = 2 // size of each dot
 
-  var canvas
 
   window.onload = function() {
-    canvas = new fabric.Canvas('game', {
+//     canvas = new fabric.Canvas('game', {
+//       backgroundColor: 'lightgray',
+//       selection: false
+//     });
+
+//     canvas.on({
+//       'mouse:move': (event) => {
+//         var p = canvas.getPointer(event.e)
+//         this.paint(p)
+//       }
+//     });
+  }
+
+
+  const Game = function(canvasId) {
+    this.canvas = new fabric.Canvas(canvasId, {
       backgroundColor: 'lightgray',
       selection: false
     });
 
-    canvas.on({
+    this.canvas.on({
       'mouse:move': (event) => {
-        var p = canvas.getPointer(event.e)
+        var p = this.canvas.getPointer(event.e)
         this.paint(p)
       }
     });
   }
+  
+  window.Game = Game;
+  Game.prototype = {}
 
-
-  const Game = {}
-
-  Game.init = function(canvas) {
-    this.canvas = canvas
-  }
-
-  Game.paint = function(coords) {
+  Game.prototype.paint = function(coords) {
     // just do a single handful
     var dotCoords
     for (var i = 0; i < paintStrength; i++) {
-      Game.drawDot(randCoords(
+      this.drawDot(randCoords(
         addCoords(coords, {x: -paintRadius, y: -paintRadius}),
         addCoords(coords, {x: paintRadius, y: paintRadius})
       ))
     }
   }
 
-  Game.drawDot = function(coords) {
-      const dot = new fabric.Circle({
-        left: coords.x,
-        top: coords.y,
-        fill: 'green',
-        radius: dotSize,
-        selectable: false
-      });
-      this.canvas.add(dot);
+  Game.prototype.drawDot = function(coords) {
+    const dot = new fabric.Circle({
+      left: coords.x,
+      top: coords.y,
+      fill: 'green',
+      radius: dotSize,
+      selectable: false
+    });
+    this.canvas.add(dot);
+  }
+  
+
+  Game.prototype.drawDot = function(coords) {
+    const dot = new fabric.Circle({
+      left: coords.x,
+      top: coords.y,
+      fill: 'green',
+      radius: dotSize,
+      selectable: false
+    });
+    this.canvas.add(dot);
   }
 
-
-
-
-  //////
-
-  function addCoords(coords1, coords2) {
-      return {
-          x: coords1.x + coords2.x,
-          y: coords1.y + coords2.y,
-      }
-  }
-
-
-  function randCoords(coords1, coords2) {
-      if (!coords2) {
-          coords2 = coords1
-          coords1 = {x: 0, y: 0}
-      }
-      return {
-          x: randInt(coords1.x, coords2.x),
-          y: randInt(coords1.y, coords2.y),
-      }
-  }
-
-  function randInt(a, b) {
-      if (typeof b === 'undefined') {
-          b = a
-          a = 0
-      }
-      return a + Math.floor(Math.random() * (b - a))
-  }
 })()
+
+
+
+//////
+
+function addCoords(coords1, coords2) {
+  return {
+    x: coords1.x + coords2.x,
+    y: coords1.y + coords2.y,
+  }
+}
+
+
+function randCoords(coords1, coords2) {
+  if (!coords2) {
+    coords2 = coords1
+    coords1 = {x: 0, y: 0}
+  }
+  return {
+    x: randInt(coords1.x, coords2.x),
+    y: randInt(coords1.y, coords2.y),
+  }
+}
+
+function randInt(a, b) {
+  if (typeof b === 'undefined') {
+    b = a
+    a = 0
+  }
+  return a + Math.floor(Math.random() * (b - a))
+}
--- public/group.js
+++ public/group.js
@@ -1,81 +1,11 @@
 // PROTOTYPE!!!!
 
-/* global fabric */
+/* global fabric, Game */
 
 
-
-const gameSize = {x: 800, y: 524}
-
-const paintRadius = 15 // region in which to paint
-const paintStrength = 2
-const paintRate = 5 // paints per second
-const paintDotSize = 2 // size of each dot
-
-var canvas
+var g
 
 window.onload = function() {
-  canvas = new fabric.Canvas('game', {
-    backgroundColor: 'lightgray',
-    selection: false
-  });
+  g = new Game('game')
   
-  canvas.on({
-    'mouse:move': function(event) {
-      var p = canvas.getPointer(event.e)
-      paint(p)
-    }
-  });
-  
-}
-
-function paint(coords) {
-    // just do a single handful
-    var dotCoords
-    for (var i = 0; i < paintStrength; i++) {
-        dotCoords = randCoords(
-            addCoords(coords, {x: -paintRadius, y: -paintRadius}),
-            addCoords(coords, {x: paintRadius, y: paintRadius})
-        )
-      
-        const dot = new fabric.Circle({
-          left: dotCoords.x,
-          top: dotCoords.y,
-          fill: 'green',
-          radius: paintDotSize,
-          selectable: false
-        });
-        canvas.add(dot);
-    }
-}
-
-
-
-
-//////
-
-function addCoords(coords1, coords2) {
-    return {
-        x: coords1.x + coords2.x,
-        y: coords1.y + coords2.y,
-    }
-}
-
-
-function randCoords(coords1, coords2) {
-    if (!coords2) {
-        coords2 = coords1
-        coords1 = {x: 0, y: 0}
-    }
-    return {
-        x: randInt(coords1.x, coords2.x),
-        y: randInt(coords1.y, coords2.y),
-    }
-}
-
-function randInt(a, b) {
-    if (typeof b === 'undefined') {
-        b = a
-        a = 0
-    }
-    return a + Math.floor(Math.random() * (b - a))
 }
